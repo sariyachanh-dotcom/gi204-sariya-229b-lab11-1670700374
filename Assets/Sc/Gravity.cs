3b063f2546cafed7d0a62f29ae6f0220d75c7f43
sariyachanh-dotcom <sariya.chanh@bumail.net>
2026-03-24 16:00:02 +0700

    Finish the earth

--- Assets/Sc/Gravity.cs
+++ Assets/Sc/Gravity.cs
@@ -1,16 +1,50 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Gravity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody rb;
+    const float G = 0.006674f;
+
+    public static List<Gravity> otherObjectsList;
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        if (otherObjectsList == null )
+        {
+            otherObjectsList = new List<Gravity>();
+        }
+        otherObjectsList.Add(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        foreach (Gravity obj in otherObjectsList)
+        {
+            if (obj != this)
+            {
+                Attract(obj);
+            }
+        }
     }
+
+    void Attract(Gravity other)
+    {
+        Rigidbody otherRB = other.rb;
+
+        Vector3 direction = rb.position - otherRB.position;
+
+        float distance = direction.magnitude;
+
+        if (distance == 0f) { return; }
+
+        float forceMagnitude = G * (rb.mass * otherRB.mass) / Mathf.Pow(distance, 2);
+
+        Vector3 gravityForce = forceMagnitude * direction.normalized;
+
+        otherRB.AddForce(gravityForce);
+    }
+
 }
